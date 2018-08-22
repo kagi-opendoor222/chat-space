fc86f38d49f24f437d28dcd827897dcf0bf53b70
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-22 19:40:42 +0900

    edit users#update  for notification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def edit; end
   def update
     if current_user.update(user_params)
+      flash[:notice] = "ユーザー情報を更新しました。"
       redirect_to root_path
     else
       render :edit
