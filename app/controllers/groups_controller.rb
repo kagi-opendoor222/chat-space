be755ebeeb1cca35ee0ba1f2d980aac7ab176997
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-23 12:38:25 +0900

    add groups#new, #create, #edi, #update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,18 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+  def create
+    Group.create(group_params)
+  end
+  def edit
+    @group = group(params[:group_id])
+  end
+  def update
+    Group.update(group_params)
+  end
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
