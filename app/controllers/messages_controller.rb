dd6bcdbe72ef794c2a119e8f720ef567d3daf7f6
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-23 12:39:14 +0900

    add message#index, #create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,14 @@
 class MessagesController < ApplicationController
+  def index
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+  end
+  def create
+    @group = Group.find(params[:group_id])
+    Messages.create(messages_params)
+  end
+  private
+  def message_params
+    params.require(:message).permit(:text)
+  end
 end
