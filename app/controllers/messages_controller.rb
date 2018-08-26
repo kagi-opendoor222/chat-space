0ce2752f17ed4173b6ff403e1ac029e4232b199f
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-26 15:32:41 +0900

    edit messages#create  to set "if not save"

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,14 @@ class MessagesController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
     @message = Message.new(message_params.merge({user_id: current_user.id, group_id: @group.id}))
+    if @message.save
+      redirect_to action: :index
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = "メッセージを入力してください"
+      render :index
+    end
+
   end
   private
   def message_params
