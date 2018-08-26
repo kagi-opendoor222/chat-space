bd038e94da0429b73325f2c6956b287c807381ed
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-26 15:32:33 +0900

    edit messages#create  to send messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,10 @@ class MessagesController < ApplicationController
   end
   def create
     @group = Group.find(params[:group_id])
-    Messages.create(messages_params)
+    @message = Message.new(message_params.merge({user_id: current_user.id, group_id: @group.id}))
   end
   private
   def message_params
-    params.require(:message).permit(:text)
+    params.require(:message).permit(:text, :image)
   end
 end
