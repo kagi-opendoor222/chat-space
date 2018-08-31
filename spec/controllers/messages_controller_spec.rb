f0ec834d48b7bd30cc4f563fc46cc0e0ca0bb08c
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-31 14:32:22 +0900

    add example to context "log in"

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -29,10 +29,35 @@ describe MessagesController do
     end
   end
   describe "POST #create" do
+    let(:params){ {user_id: user.id, group_id: group.id, message: attributes_for(:message)} }
     context "log in" do
+      before do
+        login user
+      end
       context "can save" do
+        subject {
+          post :create,
+          params: params
+        }
+        it "count up message" do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+        it "redirect to :index" do
+          expect( subject ).to redirect_to(group_messages_path(group))
+        end
       end
       context "can't save" do
+        let(:invalid_params){ {user_id: user.id, group_id: group.id, message: {text: nil, image: nil}} }
+        subject {
+          post :create,
+          params: invalid_params
+        }
+        it "does not count up" do
+          expect{ subject }.not_to change(Message, :count)
+        end
+        it "render :index" do
+          expect( subject ).to render_template(:index)
+        end
       end
     end
     context "not log in" do
