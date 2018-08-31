0c7effa0a3507c5cecd059227d43cd613d79fceb
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-31 14:27:20 +0900

    add context to "GET index"

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 describe MessagesController do
   describe "GET #index" do
+    context "log in" do
+    end
+    context "not log in" do
+    end
   end
   describe "POST #create" do
   end
