a47ea4319972912e5cc78d6a99f9c08edec6987f
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-31 14:28:48 +0900

    add example to context "log in"

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 describe MessagesController do
+  let(:user){ create(:user) }
+  let(:group){ create(:group) }
   describe "GET #index" do
     context "log in" do
+      before do
+        login user
+        get :index, group_id: group.id
+      end
+      it "assigns @message" do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+      it "assigns @group" do
+        expect(assigns(:group)).to eq group
+      end
+      it "renders :index template" do
+        expect(response).to render_template :index
+      end
     end
     context "not log in" do
     end
