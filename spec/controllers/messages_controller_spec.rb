d6c84c83ac3f9bea665337bce3a02854e39fc3a5
kagi-opendoor222 <opendoor222@gmail.com>
2018-08-31 14:29:05 +0900

    add example to context "not log in"

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 describe MessagesController do
   let(:user){ create(:user) }
   let(:group){ create(:group) }
@@ -19,6 +20,12 @@ describe MessagesController do
       end
     end
     context "not log in" do
+      before do
+        get :index, group_id: group.id
+      end
+      it "redirects to sign_up" do
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
   describe "POST #create" do
