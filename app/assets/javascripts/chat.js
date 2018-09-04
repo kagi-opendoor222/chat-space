001fe7fb31c07e63ecb2392f0254a65941fa231e
kagi-opendoor222 <opendoor222@gmail.com>
2018-09-04 13:28:37 +0900

    to display new-message in chat

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,4 +1,19 @@
 $(function(){
+  function appendList(data){
+   var html = `<li class="message-container">
+                <div class="message-container__top">
+                  ${data.user_name}
+                  <span class="date">
+                    ${data.created_at}
+                  </span>
+                </div>
+                ${(data.image != null) ? `<img style="margin-top: 10px" src="${data.image}"}` : ''}
+                <div class="message-container__message">
+                  ${data.text}
+                </div>
+              </li>`
+    $(".message-list").append(html)
+  }
   $("#new_message").on("submit", function(e){
     e.preventDefault()
     url = $(this).attr("action")
@@ -10,6 +25,9 @@ $(function(){
       dataType: "json",
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      appendList(data)
     })
   })
 })
