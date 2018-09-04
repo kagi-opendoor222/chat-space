931270734cf0e49f07552812975a038c30904d03
kagi-opendoor222 <opendoor222@gmail.com>
2018-09-04 12:53:55 +0900

    to submit formData with ajax

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,2 +1,15 @@
 $(function(){
+  $("#new_message").on("submit", function(e){
+    e.preventDefault()
+    url = $(this).attr("action")
+    var formData = new FormData(this)
+    $.ajax({
+      type: "POST",
+      data: formData,
+      url: url,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+  })
 })
