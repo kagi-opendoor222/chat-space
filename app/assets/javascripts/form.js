a4ce1120113cac35afb8ed5d93a52b4ff14a388e
kagi-opendoor222 <opendoor222@gmail.com>
2018-09-07 16:19:33 +0900

    asynchronous-connect   after keyup at user-search-field

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,2 +1,15 @@
 $(function(){
+  $("#user-search-field").on("keyup",function(){
+    var input = $("#user-search-field").val();
+    var url = "/users";
+    console.log(input);
+    $.ajax({
+      type: "GET",
+      url: url,
+      data: {
+        keyword: input
+      },
+      dataType: "json"
+    })
+  })
 })
