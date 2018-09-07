78e9bcb32f67231e7aa17c486b11b3842c978952
kagi-opendoor222 <opendoor222@gmail.com>
2018-09-07 16:33:48 +0900

    append search-result after asynchronous-connect

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,4 +1,13 @@
 $(function(){
+  function buildHtml(user){
+    var html =
+    `<div class="chat-group-user clearfix">
+       <p class="chat-group-user__name">${user.name}</p>
+       <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+     </div>`
+    $("#user-search-result").append(html)
+  }
+
   $("#user-search-field").on("keyup",function(){
     var input = $("#user-search-field").val();
     var url = "/users";
@@ -11,5 +20,14 @@ $(function(){
       },
       dataType: "json"
     })
+    .done(function(users){
+      users.forEach(function(user){
+        buildHtml(user)
+      })
+    })
+    .fail(function(users){
+      $(".alert p").text("エラーが発生しました。")
+    })
+  })
   })
 })
