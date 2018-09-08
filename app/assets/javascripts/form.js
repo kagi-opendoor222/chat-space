6a56d2708356b1009a27b00f8bb3f77e5ff6ad45
kagi-opendoor222 <opendoor222@gmail.com>
2018-09-08 14:20:28 +0900

    activate button to append group-user

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -7,6 +7,15 @@ $(function(){
      </div>`
     $("#user-search-result").append(html)
   }
+  function appendGroupUser(user){
+    var html =
+    `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+      <input name='group[user_ids][]' type='hidden' value='${user.userId}'>
+      <p class='chat-group-user__name'>${user.userName}</p>
+      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+    </div>`
+    $("#chat-group-users").append(html)
+  }
 
   $("#user-search-field").on("keyup",function(){
     var input = $("#user-search-field").val();
@@ -30,5 +39,8 @@ $(function(){
       $(".alert p").text("エラーが発生しました。")
     })
   })
+  $(document).on("click", ".chat-group-user__btn--add", function(){
+    var user = $(this).data()
+    appendGroupUser(user)
   })
 })
