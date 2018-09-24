0eec6bb1c9410ed08970e3f85c8a23da67ba0854
kagi-opendoor222 <opendoor222@gmail.com>
2018-09-24 19:22:47 +0900

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,11 +38,30 @@ set :repo_url, "git@github.com:kagi-opendoor222/chat-space.git"
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# set :rbenv_type, :user
+# set :rbenv_ruby, '2.3.1'
+# set :ssh_options, auth_methods: ['publickey'],
+#                   keys: ['/Users/Keisuke/.ssh/opendoor222_key.pem']
+# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+# set :keep_releases, 5
+
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:restart'
+#   end
+# end
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
+
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/Keisuke/.ssh/opendoor222_key.pem']
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
@@ -52,5 +71,4 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-end
 end
